5362cbb36c395e994128824f51cb8bcdca80fa17
Rene Torres <rene_edgardo_2@hotmail.com>
2024-07-10 20:11:29 -0600

    Create welcome section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/app/components/Header'
+import WelcomeSection from '@/app/components/WelcomeSection'
 
 export default async function Home() {
   return (
     <>
       <Header />
+      <WelcomeSection />
     </>
   )
 }
