cbc58754e427bdecb8aee6190d7d406a5d3e6dae
rene5394 <rene_edgardo_2@hotmail.com>
2024-10-04 22:54:08 -0600

    Set show bottom border as false for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Header from '@/app/components/Header'
-import WelcomeSection from '@/app/components/WelcomeSection'
+import Header from '@/app/components/Header/Header'
+import WelcomeSection from '@/app/components/Home/WelcomeSection'
+import Footer from '@/app/components/Footer/Footer'
+import Container from '@/app/components/Home/Container'
 
 export default async function Home() {
   return (
     <>
-      <Header />
+      <Header showBottomBorder={false} />
       <WelcomeSection />
+      <Container />
+      <Footer />
     </>
   )
 }
