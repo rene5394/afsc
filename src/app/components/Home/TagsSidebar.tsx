d359b27d8fe39385ecfe094aab5d6956dd0656bd
rene5394 <rene_edgardo_2@hotmail.com>
2024-10-09 21:57:47 -0600

    Fetch tags on TagSidebar component

--- src/app/components/Home/TagsSidebar.tsx
+++ src/app/components/Home/TagsSidebar.tsx
@@ -1,32 +1,47 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { FetchTagsUseCase } from '@/modules/tag/application/FetchTagsUseCase'
+import { TagRepository } from '@/modules/tag/infrastructure/TagRepository'
+import { Tag } from '@/modules/tag/domain/Tag'
+import { ALL_TAG } from '@/modules/tag/domain/constants'
 
 const TagsSidebar: React.FC = () => {
+  const [tags, setTags] = useState<Tag[]>([])
+  const [selectedTagId, setSelectedTagId] = useState<number>(ALL_TAG.id)
+
+  const fetchTagsUseCase = new FetchTagsUseCase(new TagRepository())
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      try {
+        const fetchedTags = await fetchTagsUseCase.execute()
+        setTags([ALL_TAG, ...fetchedTags])
+      } catch (error) {
+        console.error('Error fetching tags:', error)
+      }
+    }
+
+    fetchTags()
+  }, [])
+
+  const handleTagClick = (tagId: number) => {
+    setSelectedTagId(tagId)
+  }
+
   return (
     <div className='flex-none w-[210px] mr-8'>
-      <button className='w-full bg-red text-sm text-white py-2 mb-2'>
-        Deportation
-      </button>
-      <button className='w-full bg-black text-sm text-white py-2 mb-2'>
-        Detention
-      </button>
-      <button className='w-full bg-black text-sm text-white py-2 mb-2'>
-        War Refugee
-      </button>
-      <button className='w-full bg-black text-sm text-white py-2 mb-2'>
-        Economic Migration
-      </button>
-      <button className='w-full bg-black text-sm text-white py-2 mb-2'>
-        Climate Migration
-      </button>
-      <button className='w-full bg-black text-sm text-white py-2 mb-2'>
-        Families
-      </button>
-      <button className='w-full bg-black text-sm text-white py-2 mb-2'>
-        US Wars
-      </button>
-      <button className='w-full bg-black text-sm text-white py-2 mb-2'>
-        Civil Wars
-      </button>
+      {tags.map((tag) => (
+        <button
+          key={tag.id}
+          className={`w-full text-sm text-white py-2 mb-2 ${
+            selectedTagId === tag.id ? 'bg-red' : 'bg-black'
+          }`}
+          onClick={() => handleTagClick(tag.id)}
+        >
+          {tag.name}
+        </button>
+      ))}
     </div>
   )
 }
