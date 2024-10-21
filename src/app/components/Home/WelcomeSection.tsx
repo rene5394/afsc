b54506e9dff7f9f0b59d153eb84199ea23eeb56b
rene5394 <rene_edgardo_2@hotmail.com>
2024-10-20 18:14:57 -0600

    Refactor code using ESLint

--- src/app/components/Home/WelcomeSection.tsx
+++ src/app/components/Home/WelcomeSection.tsx
@@ -4,7 +4,9 @@ const WelcomeSection: React.FC = () => {
   return (
     <section className='bg-black text-white text-center py-24'>
       <div className='container mx-auto px-6'>
-        <h1 className='lg:text-[104px] md:text-[80px] text-[50px] leading-none'>WELCOME TO TRACES</h1>
+        <h1 className='lg:text-[104px] md:text-[80px] text-[50px] leading-none'>
+          WELCOME TO TRACES
+        </h1>
         <p className='max-w-4xl lg:text-xl text-l mx-auto'>
           Discover the narratives of migrants globally, individuals who have
           pursued a brighter future for themselves and their loved ones.
