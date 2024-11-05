a849bba08d04d8de0f10424b90d26c0808773dc7
rene5394 <rene_edgardo_2@hotmail.com>
2024-11-05 00:16:21 -0600

    Change profile breadcrumb link

--- src/app/components/Profile/Breadcrumb.tsx
+++ src/app/components/Profile/Breadcrumb.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Breadcrumb: React.FC = () => {
   return (
     <div className='container xl:max-w-[1024px] lg:max-w-[900px] md:max-w-[600px] max-w-[300px] mx-auto pt-10 pb-1'>
-      <h2 className='text-sm'>Deportation &gt; Alex &gt; Profile</h2>
+      <a href='/'>
+        <h2 className='text-sm'>⬅ Back</h2>
+      </a>
       <hr className='border-t border-gray-300 mt-2 mb-4' />
     </div>
   )
