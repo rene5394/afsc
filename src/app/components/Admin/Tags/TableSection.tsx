e37845a26cb074f8db00b79d13de8707b4e1940a
rene5394 <rene_edgardo_2@hotmail.com>
2025-04-09 23:06:48 -0600

    Add style to tag table section

--- src/app/components/Admin/Tags/TableSection.tsx
+++ src/app/components/Admin/Tags/TableSection.tsx
@@ -1,11 +1,8 @@
-import { FetchProfilesUseCase } from '@/modules/profile/application/FetchProfilesUseCase'
-import { ProfileRepository } from '@/modules/profile/infrastructure/ProfileRepository'
 import { FetchTagsUseCase } from '@/modules/tag/application/FetchTagsUseCase'
 import { TagRepository } from '@/modules/tag/infrastructure/TagRepository'
 
 import { Tag } from '@/modules/tag/domain/Tag'
 
-const fetchProfilesUseCase = new FetchProfilesUseCase(new ProfileRepository())
 const fetchTagsUseCase = new FetchTagsUseCase(new TagRepository())
 
 const fetchTags = async () => {
@@ -23,44 +20,64 @@ export default async function TableSection() {
 
   return (
     <div className='container xl:max-w-[1024px] max-w-full py-12 px-14'>
-      <h2 className='text-4xl'>TAGS</h2>
-      <hr className='border-t border-gray-300 mt-2 mb-4' />
+      <h1 className='text-5xl'>TAGS</h1>
+      <button className='my-5 px-4 py-2 bg-black text-white rounded'>
+        Add New Tag
+      </button>
       <div className='w-full overflow-x-visible overflow-y-hidden'>
-        <table className='w-full md:min-w-full table-auto md:mt-5'>
+        <table
+          id='table-tags'
+          className='w-full min-w-full table-auto border border-gray-300 border-collapse'
+        >
           <thead>
             <tr>
-              <th className='text-xl text-left whitespace-nowrap pr-4'>NAME</th>
-              <th colSpan={3} className='text-xl text-left whitespace-nowrap'>
-                <span className='ml-4'>ACTIONS</span>
+              <th className='text-xl text-left whitespace-nowrap px-6 py-3 bg-black text-white'>
+                NAME
               </th>
-            </tr>
-            <tr>
-              <th colSpan={3}>
-                <hr className='border-t border-gray-300 mt-2' />
+              <th className='text-xl text-left whitespace-nowrap px-6 py-3 bg-black text-white'>
+                STATUS
               </th>
+              <th className='text-xl text-left whitespace-nowrap px-6 py-3 bg-black text-white'>
+                ACTIONS
+              </th>
+              <th className='text-xl text-left whitespace-nowrap px-6 py-3 bg-black text-white' />
             </tr>
           </thead>
           <tbody>
             {tags.map((tag) => (
-              <tr key={tag.id}>
-                <td className='pt-6 text-sm lg:text-base whitespace-nowrap pr-4'>
+              <tr key={tag.id} className='odd:bg-white even:bg-gray-100'>
+                <td className='px-6 py-4 text-sm lg:text-base whitespace-nowrap'>
                   {tag.name}
                 </td>
-                <td className='pt-6 text-sm lg:text-base whitespace-nowrap'>
+                <td className='px-6 py-4 text-sm lg:text-base whitespace-nowrap'>
+                  {tag.active ? (
+                    <span className='text-green-500 font-bold'>
+                      {tag.active} Active
+                    </span>
+                  ) : (
+                    <span className='text-red-500 font-bold'>
+                      {tag.active} Inactive
+                    </span>
+                  )}
+                </td>
+                <td className='w-[110px] px-6 py-4 text-sm lg:text-base whitespace-nowrap'>
                   <a
                     href={`/admin/profiles/${tag.id}`}
-                    className='ml-4 px-4 py-1 bg-black text-white rounded inline-block'
+                    className='px-4 py-1 bg-black text-white rounded inline-block'
                   >
                     Edit
                   </a>
                 </td>
-                <td className='pt-6 text-sm lg:text-base whitespace-nowrap'>
-                  <a
-                    href={`/admin/tags/${tag.id}`}
-                    className='ml-4 px-4 py-1 bg-red-500 text-white rounded inline-block'
-                  >
-                    Delete
-                  </a>
+                <td className='pr-6 py-4 text-sm lg:text-base whitespace-nowrap'>
+                  {tag.active ? (
+                    <span className='px-4 py-1 bg-red-500 text-white rounded inline-block'>
+                      {tag.active} Deactivate
+                    </span>
+                  ) : (
+                    <span className='px-4 py-1 bg-green-500 text-white rounded inline-block'>
+                      {tag.active} Activate
+                    </span>
+                  )}
                 </td>
               </tr>
             ))}
