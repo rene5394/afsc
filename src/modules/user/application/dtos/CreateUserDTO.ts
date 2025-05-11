36ebe473f6c0f6d9450f085062a886aacbefb117
rene5394 <rene_edgardo_2@hotmail.com>
2025-05-10 21:04:50 -0600

    Add user module

---
+++ src/modules/user/application/dtos/CreateUserDTO.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+export const CreateUserSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  password: z.string(),
+})
+
+export type CreateUserDTO = z.infer<typeof CreateUserSchema>
