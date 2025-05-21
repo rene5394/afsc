2936b5d7a5b748b62123c3a9b496fc26c1088235
rene5394 <rene_edgardo_2@hotmail.com>
2025-05-20 19:46:54 -0600

    Add update tag endpoint

--- src/app/api/v1/tags/[id]/route.ts
+++ src/app/api/v1/tags/[id]/route.ts
@@ -1,5 +1,47 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
+import { UpdateTagSchema } from '@/modules/tag/application/dtos/UpdateTagDTO'
+
+const prisma = new PrismaClient()
+
+export async function PUT(req: NextRequest) {
+  try {
+    const id = Number(req.nextUrl.pathname.split('/')[4])
+    const tag = await prisma.profile.findUnique({ where: { id } })
+
+    if (!tag) {
+      return NextResponse.json(
+        { status: 404, message: 'Tag not found' },
+        { status: 404 }
+      )
+    }
+
+    const body = await req.json()
+    const parsed = UpdateTagSchema.safeParse(body)
+
+    if (!parsed.success) {
+      return NextResponse.json(
+        { message: 'Invalid data', errors: parsed.error.errors },
+        { status: 400 }
+      )
+    }
+
+    const updatedTag = await prisma.tag.update({
+      where: { id },
+      data: parsed.data,
+    })
+
+    return NextResponse.json(
+      { status: 200, message: 'Tag updated successfully', data: updatedTag },
+      { status: 200 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      { status: 500, message: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
 
 export async function DELETE(req: NextRequest) {
   try {
